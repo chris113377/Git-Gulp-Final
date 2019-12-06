a8fa819200d223570c21cef8c4b41651bbbce993
Christian <chris113377@gmail.com>
2019-12-06 09:42:22 -0600

    installed and optimized images using imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const {src, dest, series, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const prefix = require('gulp-autoprefixer');
 const miniCSS = require('gulp-clean-css');
-
+const imagemin = require('gulp-imagemin');
 
 function html() {
     return src('./src/*.html').pipe(dest('./dest'))
@@ -19,5 +19,16 @@ function css() {
       .pipe(browserSync.stream());
 }
 
+function image() {
+    return src('./src/images/*')
+      .pipe(imagemin([
+          imagemin.optipng({optimizationLevel: 5}),
+          imagemin.jpegtran({progressive: true})
+      ]))
+      .pipe(dest('./dest/images'))
+      .pipe(browserSync.stream());
+}
+
 exports.html = html;
 exports.css = css;
+exports.image = image;
