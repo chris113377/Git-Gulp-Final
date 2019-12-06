7817f0e476fbd7d2b2725146d9de6df6f417d28c
Christian <chris113377@gmail.com>
2019-12-06 09:35:47 -0600

    installed and initialized css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 const {src, dest, series, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const prefix = require('gulp-autoprefixer');
+const miniCSS = require('gulp-clean-css');
+
 
 function html() {
     return src('./src/*.html').pipe(dest('./dest'))
     .pipe(browserSync.stream());
 }
 
+function css() {
+    return src('./src/css/**/*.css')
+      .pipe(prefix({
+          cascade: false
+      }))
+      .pipe(miniCSS())
+      .pipe(dest('./dest/css/'))
+      .pipe(browserSync.stream());
+}
 
 exports.html = html;
+exports.css = css;
